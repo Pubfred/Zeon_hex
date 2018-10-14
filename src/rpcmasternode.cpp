97beb573bf128e73c6f6f7b6d628b8e7b8b60197
Pubfred <pubfree@free.fr>
2018-10-14 10:59:03 +0200

    Update rpcmasternode.cpp

--- src/rpcmasternode.cpp
+++ src/rpcmasternode.cpp
@@ -167,7 +167,7 @@ UniValue masternode(const UniValue& params, bool fHelp)
             "  debug        - Print masternode status\n"
             "  genkey       - Generate new masternodeprivkey\n"
             "  outputs      - Print masternode compatible outputs\n"
-            "  start        - Start masternode configured in xdna.conf\n"
+            "  start        - Start masternode configured in zeon.conf\n"
             "  start-alias  - Start single masternode by assigned alias configured in masternode.conf\n"
             "  start-<mode> - Start masternodes configured in masternode.conf (<mode>: 'all', 'missing', 'disabled')\n"
             "  status       - Print masternode status information\n"
