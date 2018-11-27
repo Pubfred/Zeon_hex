5e1789f1f92e6f47b3a9ed2d9e183e927a03303d
root <pubfree@free.fr>
2018-11-27 16:24:45 +0100

    leveldb warning

--- src/config/zeon-config.h
+++ src/config/zeon-config.h
@@ -274,7 +274,7 @@
 #define PACKAGE_NAME "ZEON Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "ZEON Core 2.1.0"
+#define PACKAGE_STRING "ZEON Core 2.1.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "zeon"
@@ -283,7 +283,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.1.0"
+#define PACKAGE_VERSION "2.1.1"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
