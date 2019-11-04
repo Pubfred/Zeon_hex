99bb288720a76cfabe840e21d0841eeee65f7913
Pubfred <zeonmymail@gmail.com>
2019-11-04 19:34:03 +0100

    GetMasternodePayment

--- src/main.h
+++ src/main.h
@@ -228,7 +228,7 @@ bool DisconnectBlocksAndReprocess(int blocks);
 
 // ***TODO***
 double ConvertBitsToDouble(unsigned int nBits);
-int64_t GetMasternodePayment(int nHeight, unsigned mnlevel, int64_t blockValue);
+int64_t GetMasternodePayment(int nHeight, unsigned mnlevel, int64_t blockValue, bool bDrift = false );
 
 bool ActivateBestChain(CValidationState& state, CBlock* pblock = NULL);
 CAmount GetBlockValue(int nHeight, uint32_t nTime);
