32172f24dd7ee97f8c0b1ebdc8942e7e78b5a593
Pubfred <zeonmymail@gmail.com>
2019-11-28 20:14:14 +0100

    BlockReward

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -355,7 +355,7 @@ void OverviewPage::updatBlockChainInfo()
 
     int CurrentBlock = chainActive.Height();
     int64_t netHashRate = chainActive.GetNetworkHashPS(24, CurrentBlock);
-    int64_t BlockReward = Params().SubsidyValue(netHashRate, tip_time);
+    double BlockReward = GetBlockValue(CurrentBlock);
     double BlockRewardZEON =  static_cast<double>(BlockReward)/static_cast<double>(COIN);
     //int64_t ZEONSupply = chainActive.Tip()->nMoneySupply / COIN;
 
