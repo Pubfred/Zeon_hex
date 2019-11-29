22be7695d48843accd016ba2fd49eab1a708ea82
Pubfred <zeonmymail@gmail.com>
2019-11-29 14:05:15 +0100

    blockHeight

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -320,7 +320,7 @@ unsigned CMasternode::Level(CAmount vin_val, int blockHeight)
           case 15000  * COIN: return 2;
           case 25000  * COIN: return 3;
         }
-    } else if (blockHeight >= 1700000‬ && blockHeight < 2550000 ) {
+    } else if (blockHeight >= 1700000 && blockHeight < 2550000 ) {
         switch(vin_val) {
           case 10000  * COIN: return 1;
           case 30000  * COIN: return 2;
